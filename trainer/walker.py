019c6c57f836a1d3729bc7d4cb72bc6b291b2cc8
Jean-Baptiste Courtois <jbaptistec2003@gmail.com>
2023-08-28 23:10:05 +0200

    Exclude local spots

--- trainer/walker.py
+++ trainer/walker.py
@@ -1,4 +1,8 @@
+import os
 from glob import glob
 
 DEFAULT_FILE = './tests/fixtures/toy_game_AKK7.txt'
-FILES = glob('Spots/**/*.txt', recursive=True) or [DEFAULT_FILE]
+FILES = [
+	file for file in glob('Spots/**/*.txt', recursive=True)
+	if 'local' not in file.split(os.sep)
+] or [DEFAULT_FILE]
