577ee926632252f63c5c6d543281461116deb914
Jean-Baptiste Courtois <jean-baptiste.courtois@sap.com>
2024-02-13 23:45:09 +0100

    Revert "Exclude local spots"
    This reverts commit 019c6c57f836a1d3729bc7d4cb72bc6b291b2cc8.

--- trainer/walker.py
+++ trainer/walker.py
@@ -1,8 +1,4 @@
-import os
 from glob import glob
 
 DEFAULT_FILE = './tests/fixtures/toy_game_AKK7.txt'
-FILES = [
-	file for file in glob('Spots/**/*.txt', recursive=True)
-	if 'local' not in file.split(os.sep)
-] or [DEFAULT_FILE]
+FILES = glob('Spots/**/*.txt', recursive=True) or [DEFAULT_FILE]
